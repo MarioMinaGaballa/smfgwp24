2e28040d7a00887e25bcf50e2affb31af777fa02
MarioMinaGaballa <mariomena199@gmail.com>
2024-09-11 02:07:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import './App.css'; 
+import logo1 from '../src/imgs/Logo.png'
+import logo2 from '../src/imgs/png logo.png'
+import CountDown from './components/CountDown/CountDown'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+    <div className="logo-container">
+      <img src={logo2} alt="logo2" className="logo1" />
+      <img src={logo1} alt="logo1" className="logo2" />
+    
+      </div>
+      <div className="text-container">
+      <h1 className='centered-text'>Class 2024 - 11 oct 6Am</h1>
+      <div className='centered-text'>اسرة مارمينا الهندسية</div>
+      </div>
+      <CountDown/>
     </div>
   );
 }
